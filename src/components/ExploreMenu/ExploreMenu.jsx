082cb30e4dfdd1d420737e49111958059f8e4208
SeiamAlMahmud <samahmud49@gmail.com>
2024-06-15 14:41:45 +0000

    improve header section

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './exploreMenu.css'
 import { menu_list } from '../../assets/assets'
 const ExploreMenu = ({ category, setCategory }) => {
+    const endRef = useRef(null);
+    useEffect(()=>{
+      endRef.current?.scrollIntoView({behavior: 'smooth'})
+    },[])
     return (
         <div className='explore-menu' id='explore-menu'>
             <h1>Explore our menu</h1>
@@ -9,11 +13,14 @@ const ExploreMenu = ({ category, setCategory }) => {
             <div className="explore-menu-list">
                 {
                     menu_list.map((item, idx) => {
+                       
                         return (
+                            <>
                             <div onClick={() => setCategory(prev => prev === item.menu_name ? "All" : item.menu_name)} key={idx} className="explore-menu-list-item">
                                 <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt="" />
-                                <p>{item.menu_name}</p>
+                                <p >{item.menu_name}</p>
                             </div>
+                            </>
                         )
                     })
                 }
